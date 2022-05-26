61388d86c2e66c69b40c9d1847c298e49c0b2fc2
Victor Luz <85527775+vitinop@users.noreply.github.com>
2022-05-26 01:09:47 -0300

    updates

--- checkpoint1-POO-JAVA/src/Program.java
+++ checkpoint1-POO-JAVA/src/Program.java
@@ -6,15 +6,32 @@ import entities.Disciplina;
 
 public class Program {
    public static void main(String[] args){
+       System.out.println("Instância do primeiro Professor");
        Aluno a1 = new Aluno( "Lopes","Manoel",true,"000000");
 
        System.out.println(a1);
 
+       //
+
+       System.out.println("Instância do primeiro Aluno");
        Professor p1 = new Professor("Lopes", true, "Manoel","Matemática","00001");
 
        p1.nota(7.0);
 
        System.out.println(p1);
+
+       //
+
+       System.out.println("Instância de primeira Turma");
+       Professor t1 = new Turma;
+       System.out.println(t1);
+
+
+
+       //
+       System.out.println("Instância de primeira disciplina");
+       Professor d1 = new Disciplina();
+       System.out.println(d1);
 
 
    }
